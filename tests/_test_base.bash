3fa4ccadf64e915991a1e92b57abfa8b67b739a3
mitsuru793 <mitsuru793@gmail.com>
2019-02-07 21:52:19 +0900

    add function debug for test

--- tests/_test_base.bash
+++ tests/_test_base.bash
@@ -2,3 +2,8 @@ source "$SECRET_PROJECT_ROOT/main.sh"
 
 string_inputs=("" +1 "+1" a "a" "abc" "漢字" "あ")
 integer_inputs=(-11 -1 0 1 11 "-11" "0" "11")
+
+debug() {
+  local text=$1
+  echo -e "\e[32m${text}\e[m"
+}
